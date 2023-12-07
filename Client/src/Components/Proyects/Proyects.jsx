65803d0e9b9025f538a38c144d007ac18b23f306
Andres Riera <andresriera9999@gmail.com>
2023-12-07 12:20:21 -0300

    PortalDist

--- Client/src/Components/Proyects/Proyects.jsx
+++ Client/src/Components/Proyects/Proyects.jsx
@@ -2,6 +2,7 @@ import styles from "./Proyects.module.css"
 import {  useSelector } from "react-redux";
 import HenryFoods from "../../Assets/Proyectos/HenryFoods.webp"
 import PatitasSinHogar from "../../Assets/Proyectos/PatitasSinHogar.webp"
+import PortalDistribuidora from "../../Assets/Proyectos/PortalDistribuidora.webp"
 import LogoGitHubLight from "../../Assets/Logo/LogoGitHubLight.png"
 import LogoGitHubDark from "../../Assets/Logo/LogoGitHubDark.png"
 import LogoNetLight from "../../Assets/Logo/LogoNetLight.png"
@@ -62,7 +63,31 @@ function Proyects() {
         <p className={Dark ? styles.subTitleDark : styles.subTitleLight}>{Language ? "|| This are some proyects where I worked" : "|| Estos son algunos de los proyectos en los que e trabajado"}</p>
       </div>
       <div className={styles.proyecto}>
-        <h1 className={styles.proyectTitle}>{Language ? "Recipes App" : "Aplicación de recetas"}</h1>
+        <p className={styles.proyectTitle}>Portal Distribuidora</p>
+        <p className={Dark ? styles.proyectDescriptionDark : styles.proyectDescriptionLight}>{Language ? "Portal Distribuidora is a local business who asked me to develop for them a landing page to show the world who they are and how they work. In this case I developed the page with a colleague. On the website you can contact them through a form which is sent to the company via email and I used the Google Maps API tho show the location of the branch offices." : "Portal Distribuidora es una empresa local a la cual desarrolle una Landing Page informativa para mostrar al mundo como trabajan y quienes son. En este caso el projecto fue desarrollado en conjunto con un colega. A travez de la pagina se puede mantener contacto con la empresa mediante un formulario el cual se envia al mail de la misma, tambien se implemento la API de Google Maps para moder mostrar la ubicacion de todas las sucursales que esta empresa tiene."}
+        </p>
+        <div className={styles.imgAndLogo}>
+            <img src={PortalDistribuidora} alt="" className={styles.proyectImg}></img>
+            <a href="https://github.com/JaviZaldu/DistribuidoraPortal" target="_blank" rel="noopener noreferrer"><img src={Dark ? LogoGitHubDark : LogoGitHubLight} alt="" className={styles.logo}></img></a>
+            <a href="https://distribuidoraportal.com/" target="_blank" rel="noopener noreferrer"><img src={Dark ? LogoNetDark : LogoNetLight} alt="" className={styles.logo}></img></a>
+            <p className={Dark ? styles.techDark : styles.techLight}>React | Redux | Node | NodeMailer | GoogleMaps</p>
+        </div>
+      </div>
+      <div className={styles.proyecto}>
+      <p className={styles.proyectTitle}>Patitas sin Hogar</p>
+        <p className={Dark ? styles.proyectDescriptionDark : styles.proyectDescriptionLight}>{Language ? "Patitas sin hogar is a website where our main objetive is to help people to adopt pets without home, with this website we make an easier contact between both sides. Each user can upload as many pets as they want to their profile and they will be visible to any other user who want to adopt a pet. The website has a review and donations section. For the website owner it has an admin dashboard where you can fully manage users , reviews ,publications and donations" : "Patitas sin hogar es una aplicacion web en la cual fomentamos y ayudamos a personas a adoptar mascotas que no tienen hogar, esto lo hacemos facilitando el contacto entre la persona adoptante y la que da en adopcion. Cada usuario podra cargar cuantas mascotas quiera a su perfil y seran visibles para cualquier usuario que este buscando un nuevo compañero. Cuenta con una seccion de reseñas y de donaciones, como asi tambien un dashboard de administrador para gestionar publicaciones, usuarios, reseñas y donaciones"}
+        </p>
+        <p className={Dark ? styles.proyectDescriptionDark : styles.proyectDescriptionLight}>
+            {Language ? "In this opportunity I integrate the FrontEnd team, using methodologies such as Scrum, Git and GitFLow. This proyect was part of SoyHenry Bootcamp" : "En esta oportunidad integre el equipo de FrontEnd, utilizando metodologia Scrum y herramientas como Git y GitFlow. Este proyecto fue parte de SoyHenry Bootcamp"}
+        </p>
+        <div className={styles.imgAndLogo}>
+            <img src={PatitasSinHogar} alt="" className={styles.proyectImg}></img>
+            <a href="https://github.com/RieraAndres/Henry_PF.git" target="_blank" rel="noopener noreferrer"><img src={Dark ? LogoGitHubDark : LogoGitHubLight} alt="" className={styles.logo}></img></a>
+            <a href="https://henry-pf-git-main-rieraandres.vercel.app/" target="_blank" rel="noopener noreferrer"><img src={Dark ? LogoNetDark : LogoNetLight} alt="" className={styles.logo}></img></a>
+            <p className={Dark ? styles.techDark : styles.techLight}>React | Redux | Express | PostgreSQL | Sequilize | Node | Bootstrap | Scrum | Cloudinary | MercadoPagoApi | NodeMailer | GoogleAuth</p>
+        </div>
+      <div className={styles.proyecto} style={{paddingTop:"5%"}}>
+        <p className={styles.proyectTitle}>{Language ? "Recipes App" : "Aplicación de recetas"}</p>
         <p className={Dark ? styles.proyectDescriptionDark : styles.proyectDescriptionLight}>
           {Language ? "An App where you can find a lot of recipes coming from an external API, each recipe has a detail page where you can find nutritional information and how to cook them step by step. Users can filter them by diet type, search by name, order alphabetically and by health score from 1 to 100. In addition users can create their own recipe and upload it to the website" : " Una aplicación que consume datos de una API, la cual permite la visualización de recetas,con su respectiva información nutricional detallada y paso a paso para su creación. Entre algunas funcionalidades, el usuario puede filtrar las recetas por el tipo de dieta que quiera realizar, puede buscar por nombre y ordenarlas por orden alfabético y también por su nivel saludable. Tambien se le da la posibilidad al usuario de cargar su propia receta"}
         </p>
@@ -76,19 +101,6 @@ function Proyects() {
             <p className={Dark ? styles.techDark : styles.techLight}>React | Redux | Express | PostgreSQL | Sequilize | Node </p>
         </div>
       </div>
-      <div className={styles.proyecto}>
-      <h1 className={styles.proyectTitle}>Patitas sin Hogar</h1>
-        <p className={Dark ? styles.proyectDescriptionDark : styles.proyectDescriptionLight}>{Language ? "Patitas sin hogar is a website where our main objetive is to help people to adopt pets without home, with this website we make an easier contact between both sides. Each user can upload as many pets as they want to their profile and they will be visible to any other user who want to adopt a pet. The website has a review and donations section. For the website owner it has an admin dashboard where you can fully manage users , reviews ,publications and donations" : "Patitas sin hogar es una aplicacion web en la cual fomentamos y ayudamos a personas a adoptar mascotas que no tienen hogar, esto lo hacemos facilitando el contacto entre la persona adoptante y la que da en adopcion. Cada usuario podra cargar cuantas mascotas quiera a su perfil y seran visibles para cualquier usuario que este buscando un nuevo compañero. Cuenta con una seccion de reseñas y de donaciones, como asi tambien un dashboard de administrador para gestionar publicaciones, usuarios, reseñas y donaciones"}
-        </p>
-        <p className={Dark ? styles.proyectDescriptionDark : styles.proyectDescriptionLight}>
-            {Language ? "In this opportunity I integrate the FrontEnd team, using methodologies such as Scrum, Git and GitFLow. This proyect was part of SoyHenry Bootcamp" : "En esta oportunidad integre el equipo de FrontEnd, utilizando metodologia Scrum y herramientas como Git y GitFlow. Este proyecto fue parte de SoyHenry Bootcamp"}
-        </p>
-        <div className={styles.imgAndLogo}>
-            <img src={PatitasSinHogar} alt="" className={styles.proyectImg}></img>
-            <a href="https://github.com/RieraAndres/Henry_PF.git" target="_blank" rel="noopener noreferrer"><img src={Dark ? LogoGitHubDark : LogoGitHubLight} alt="" className={styles.logo}></img></a>
-            <a href="https://henry-pf-git-main-rieraandres.vercel.app/" target="_blank" rel="noopener noreferrer"><img src={Dark ? LogoNetDark : LogoNetLight} alt="" className={styles.logo}></img></a>
-            <p className={Dark ? styles.techDark : styles.techLight}>React | Redux | Express | PostgreSQL | Sequilize | Node | Bootstrap | Scrum | Cloudinary | MercadoPagoApi | NodeMailer | GoogleAuth</p>
-        </div>
       </div>
     </motion.div>
   );
